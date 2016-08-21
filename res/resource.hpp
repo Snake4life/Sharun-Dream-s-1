a3eced63f542ad29de9422435a31299b47705061
Clofriwen <clofriwen@gmail.com>
2016-08-21 18:57:01 +0200

    res dir

---
+++ res/resource.hpp
@@ -0,0 +1,9 @@
+#ifndef IDC_STATIC
+  #define IDC_STATIC                   -1
+#endif
+
+#define STR_VERSION                    980
+#define WINDOW_TITLE                   981
+
+#define IDI_ICON_App                   982
+#define IDI_ICON_Sub                   983
